4c14737b62b1f7910345996907e3520c79a5d420
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-09-10 14:45:43 -0300

    Se crean todas las estructuras referentes a las claderas (JSON, CONTROLADORAS y RUTAS)

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-const clientesRuta = require('./rutas/rutaClientes');
-
-app.use(clientesRuta);
 
 app.listen(3001, () => {
     console.log('Aplicacion corriendo en el puerto 3001')
@@ -14,7 +11,10 @@ app.listen(3001, () => {
 app.get('/', (request, response) => {
     response.sendFile(path.resolve(__dirname, 'index.html'))
 })
-
+const clientesRuta = require('./rutas/rutaClientes');
+const calderasRuta = require('./rutas/rutaCalderas');
+app.use(clientesRuta);
+app.use('/api/calderas', calderasRuta());
 
 
 
