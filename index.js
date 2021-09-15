9597c930b7c5693a29b0cb2806ec71b45d804046
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-09-15 17:04:10 -0300

    Se instala paquete mongoose y se conecta con base de datos en MongoDBAtlas

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 /* Body-Parser */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3001, () => {
-    console.log('Aplicacion corriendo en el puerto 3001')
+mongoose.connect('mongodb+srv://GonzaloDeCastro:12ec5DciPaAA7Z7U@cluster0.ca39s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+.then((result) => {
+    console.log(`Base de datos conectada`)
 })
+.catch((error) => {
+    console.log(`Base de datos no conectada, error ${error}`)
+})
+
+
+app.listen(3001, () => {
+    console.log('Aplicacion corriendo en el puerto 3001');
+});
 
 //Routes o rutas
 app.get('/', (request, response) => {
-    response.sendFile(path.resolve(__dirname, 'index.html'))
-})
-<<<<<<< HEAD
-const tecnicosRuta = require('./rutas/rutaTecnicos');
-
-
-=======
+    response.sendFile(path.resolve(__dirname, 'index.html'));
+});
 
 
 
-const tecnicosRuta = require('./rutas/rutaTecnicos');
 const clientesRuta = require('./rutas/rutaClientes');
 const calderasRuta = require('./rutas/rutaCalderas');
+const tecnicosRuta = require('./rutas/rutaTecnicos');
 
 app.use('/api/clientes', clientesRuta());
 app.use('/api/calderas', calderasRuta());
->>>>>>> 53e281e477ab527a2517fe0e41f7822ade71f197
 app.use('/api/tecnicos', tecnicosRuta());
 
 
