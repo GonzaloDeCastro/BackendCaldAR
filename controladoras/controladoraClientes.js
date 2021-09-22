d2d3cd44f857b825e2a8accca95dafd2b0fa51b9
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-09-22 00:48:18 -0300

    Se configuran prettier y eslint, y se limpia codigo

--- controladoras/controladoraClientes.js
+++ controladoras/controladoraClientes.js
@@ -3,19 +3,22 @@ const fs = require('fs');
 // Crear una Nuevo Cliente
 exports.addNewcliente = async (req, res) => {
   try {
-    
     const { direccion, tipo } = req.body; //data from POSTMAN
 
     let clienteJSON = fs.readFileSync('datos/clientes.json', 'utf8'); //data from JSON file
     let clientes = JSON.parse(clienteJSON);
-    if (!clientes) return res.status(400).json('Json inexistente.');  //error no JSON file
-    
+    if (!clientes) return res.status(400).json('Json inexistente.'); //error no JSON file
+
     if (!direccion) {
-      return res.status(400).send({ error: 'No ingreso direccion de la cliente.' });
+      return res
+        .status(400)
+        .send({ error: 'No ingreso direccion de la cliente.' });
     }
 
     if (!tipo) {
-      return res.status(400).send({ error: 'No especifico el tipo de cliente.' });
+      return res
+        .status(400)
+        .send({ error: 'No especifico el tipo de cliente.' });
     }
 
     const clienteId = Number(clientes[clientes.length - 1].id) + 1;
@@ -24,18 +27,14 @@ exports.addNewcliente = async (req, res) => {
 
     fs.writeFileSync('datos/clientes.json', JSON.stringify(clientes), {
       encoding: 'utf8',
-      flag: 'w',
+      flag: 'w'
     });
 
     return res.status(200).json(clientes);
-
   } catch (error) {
-
-    console.error(error);  //error
-
+    console.error(error); //error
   }
 };
-
 
 // Obtener clientes
 exports.getAllclientes = async (req, res) => {
@@ -48,7 +47,6 @@ exports.getAllclientes = async (req, res) => {
     console.error(error);
   }
 };
-
 
 // Obtener Cliente por ID
 exports.getclienteById = async (req, res) => {
@@ -70,7 +68,6 @@ exports.getclienteById = async (req, res) => {
   }
 };
 
-
 // Obtener Cliente por tipo (A, B, C, o D)
 exports.getclienteByTipo = async (req, res) => {
   try {
@@ -78,72 +75,71 @@ exports.getclienteByTipo = async (req, res) => {
     let clienteJSON = fs.readFileSync('datos/clientes.json', 'utf8');
     let clientes = JSON.parse(clienteJSON);
 
-    let cliente = clientes.filter(
-      (cliente) => cliente.tipo === clienteTipo
-    );
+    let cliente = clientes.filter((cliente) => cliente.tipo === clienteTipo);
 
     if (cliente.length === 0)
-      return res.status(400).json(`No se encontraron clientes del tipo ${clienteTipo}`);
+      return res
+        .status(400)
+        .json(`No se encontraron clientes del tipo ${clienteTipo}`);
 
     return res.status(200).json(cliente);
-
   } catch (error) {
     console.error(error);
     return res.status(500).json('Internal server error.');
   }
 };
 
-
 // UPDATE Or MODIFY A cliente By ID
 exports.updatecliente = async (req, res) => {
   try {
-    
     const { id, direccion, tipo } = req.body;
-    
+
     let clienteJSON = fs.readFileSync('datos/clientes.json', 'utf8');
 
     let clientes = JSON.parse(clienteJSON);
     if (!clientes) return res.status(400).json('Json Inexistente.');
-    
+
     let clienteIndex = clientes.findIndex(
       (cliente) => Number(cliente.id) === Number(id)
     );
-    
+
     if (clienteIndex === -1) {
       return res
         .status(400)
         .send({ error: `No existe la cliente con id: ${clienteId} .` });
     }
-    
+
     if (!direccion) {
-      return res.status(400).send({ error: 'No ingreso descripcion de la cliente.' });
+      return res
+        .status(400)
+        .send({ error: 'No ingreso descripcion de la cliente.' });
     }
 
     if (!tipo) {
-      return res.status(400).send({ error: 'No especifico el tipo de cliente.' });
+      return res
+        .status(400)
+        .send({ error: 'No especifico el tipo de cliente.' });
     }
 
     const updatedcliente = {
       id: Number(id),
       direccion,
-      tipo,
+      tipo
     };
 
     clientes[clienteIndex] = updatedcliente;
 
     fs.writeFileSync('datos/clientes.json', JSON.stringify(clientes), {
       encoding: 'utf8',
-      flag: 'w',
+      flag: 'w'
     });
 
     return res.status(200).json(clientes);
-
   } catch (error) {
     console.error(error);
     return res.status(500).json('Internal server error.');
   }
 };
-
 
 // DELETE A cliente By ID
 exports.deletecliente = async (req, res) => {
@@ -169,11 +165,10 @@ exports.deletecliente = async (req, res) => {
 
     fs.writeFileSync('datos/clientes.json', JSON.stringify(clientes), {
       encoding: 'utf8',
-      flag: 'w',
+      flag: 'w'
     });
 
     return res.status(200).json(clientes);
-
   } catch (error) {
     console.error(error);
     return res.status(500).json('Internal server error.');
--- controladoras/controladoraTecnicos.js
+++ controladoras/controladoraTecnicos.js
@@ -3,195 +3,217 @@ const fs = require('fs');
 // Crear una Nuevo Tecnico
 exports.addNewTecnico = async (req, res) => {
   try {
-    
     const { nombre, dni, telefono, correo, direccion, tipo } = req.body; //data from POSTMAN
 
     let tecnicoJSON = fs.readFileSync('datos/tecnicos.json', 'utf8'); //data from JSON file
     let tecnicos = JSON.parse(tecnicoJSON);
 
-    if (!tecnicos) return res.status(400).json('Json inexistente.');  //error no JSON file
+    if (!tecnicos) return res.status(400).json('Json inexistente.'); //error no JSON file
 
     if (!nombre) {
-        return res.status(400).send({ error: 'No ingreso el nombre del Técnico.' });
-    }   
+      return res
+        .status(400)
+        .send({ error: 'No ingreso el nombre del Técnico.' });
+    }
     if (!dni) {
-        return res.status(400).send({ error: 'No ingreso el dni del Técnico.' });
+      return res.status(400).send({ error: 'No ingreso el dni del Técnico.' });
     }
     if (!telefono) {
-        return res.status(400).send({ error: 'No ingreso el telefono del Técnico.' });
-    } 
+      return res
+        .status(400)
+        .send({ error: 'No ingreso el telefono del Técnico.' });
+    }
     if (!correo) {
-        return res.status(400).send({ error: 'No ingreso el correo del Técnico.' });
-    } 
+      return res
+        .status(400)
+        .send({ error: 'No ingreso el correo del Técnico.' });
+    }
     if (!direccion) {
-        return res.status(400).send({ error: 'No ingreso la dirección del Técnico.' });
-    }    
+      return res
+        .status(400)
+        .send({ error: 'No ingreso la dirección del Técnico.' });
+    }
     if (!tipo) {
-        return res.status(400).send({ error: 'No ingreso el tipo de Técnico.' });
+      return res.status(400).send({ error: 'No ingreso el tipo de Técnico.' });
     }
 
     const tecnicoId = Number(tecnicos[tecnicos.length - 1].id) + 1;
-    const newTecnico = { id: tecnicoId, nombre, dni, telefono, correo, direccion, tipo };
+    const newTecnico = {
+      id: tecnicoId,
+      nombre,
+      dni,
+      telefono,
+      correo,
+      direccion,
+      tipo
+    };
     tecnicos.push(newTecnico);
 
     fs.writeFileSync('datos/tecnicos.json', JSON.stringify(tecnicos), {
-        encoding: 'utf8',
-        flag: 'w',
+      encoding: 'utf8',
+      flag: 'w'
     });
 
     return res.status(200).json(tecnicos);
-
-    } catch (error) {
-        console.error(error);
-        return res.status(404).json('Error Interno del Servidor.');
-    }
+  } catch (error) {
+    console.error(error);
+    return res.status(404).json('Error Interno del Servidor.');
+  }
 };
-
 
 exports.getAlltecnicos = async (req, res) => {
-    try {
-        let tecnicoJSON = fs.readFileSync('datos/tecnicos.json', 'utf8');
-        let tecnicos = JSON.parse(tecnicoJSON);
-        if (!tecnicos) return res.status(400).json('Json inexistente.');
-        return res.status(200).json(tecnicos);
-    } catch (error) {
-        console.error(error);
-        return res.status(404).json('Error Interno del Servidor.');
-    }
+  try {
+    let tecnicoJSON = fs.readFileSync('datos/tecnicos.json', 'utf8');
+    let tecnicos = JSON.parse(tecnicoJSON);
+    if (!tecnicos) return res.status(400).json('Json inexistente.');
+    return res.status(200).json(tecnicos);
+  } catch (error) {
+    console.error(error);
+    return res.status(404).json('Error Interno del Servidor.');
+  }
 };
-
 
 exports.getTecnicoById = async (req, res) => {
-    try {
-        const tecnicoId = req.params.tecnicoId;
-        let tecnicoJSON = fs.readFileSync('datos/tecnicos.json', 'utf8');
-        let tecnicos = JSON.parse(tecnicoJSON);
+  try {
+    const tecnicoId = req.params.tecnicoId;
+    let tecnicoJSON = fs.readFileSync('datos/tecnicos.json', 'utf8');
+    let tecnicos = JSON.parse(tecnicoJSON);
 
-        let tecnico = tecnicos.filter(
-            (tecnico) => Number(tecnico.id) === Number(tecnicoId)
-        );
+    let tecnico = tecnicos.filter(
+      (tecnico) => Number(tecnico.id) === Number(tecnicoId)
+    );
 
-        if (tecnico.length === 0)
-            return res.status(400).json('No se encontro un Técnico con ese Id.'); 
+    if (tecnico.length === 0)
+      return res.status(400).json('No se encontro un Técnico con ese Id.');
 
-        return res.status(200).json(tecnico);
-    } catch (error) {
-        console.error(error);
-        return res.status(404).json('Error Interno del Servidor.');
-    }
+    return res.status(200).json(tecnico);
+  } catch (error) {
+    console.error(error);
+    return res.status(404).json('Error Interno del Servidor.');
+  }
 };
-
 
 // Obtener Tecnico por tipo (A, B, C o D)
 exports.getTecnicoByTipo = async (req, res) => {
-    try {
-        const tecnicoTipo = req.params.tecnicoTipo;
-        let tecnicoJSON = fs.readFileSync('datos/tecnicos.json', 'utf8');
-        let tecnicos = JSON.parse(tecnicoJSON);
+  try {
+    const tecnicoTipo = req.params.tecnicoTipo;
+    let tecnicoJSON = fs.readFileSync('datos/tecnicos.json', 'utf8');
+    let tecnicos = JSON.parse(tecnicoJSON);
 
-        let tecnico = tecnicos.filter(
-            (tecnico) => tecnico.tipo === tecnicoTipo
-        );
+    let tecnico = tecnicos.filter((tecnico) => tecnico.tipo === tecnicoTipo);
 
-        if (tecnico.length === 0)
-            return res.status(400).json(`No se encontraron Técnicos del tipo ${tecnicoTipo}`);
+    if (tecnico.length === 0)
+      return res
+        .status(400)
+        .json(`No se encontraron Técnicos del tipo ${tecnicoTipo}`);
 
-        return res.status(200).json(tecnico);
-    } catch (error) {
-        console.error(error);
-        return res.status(404).json('Error Interno del Servidor.');
-    }
+    return res.status(200).json(tecnico);
+  } catch (error) {
+    console.error(error);
+    return res.status(404).json('Error Interno del Servidor.');
+  }
 };
-
 
 // UPDATE Or MODIFY A Tecnico By ID
 exports.updateTecnico = async (req, res) => {
-    try {
-    
-        const { id, nombre, dni, telefono, correo, direccion, tipo } = req.body;
-    
-        let tecnicoJSON = fs.readFileSync('datos/tecnicos.json', 'utf8');
+  try {
+    const { id, nombre, dni, telefono, correo, direccion, tipo } = req.body;
 
-        let tecnicos = JSON.parse(tecnicoJSON);
-        if (!tecnicos) return res.status(400).json('Json Inexistente.');
-    
-        let tecnicoIndex = tecnicos.findIndex(
-            (tecnico) => Number(tecnico.id) === Number(id)
-        );
-    
-        if (tecnicoIndex === -1) {
-            return res.status(400).send({ error: `No existe el Técnico con id: ${tecnicoId}.` });
-        }
-        if (!nombre) {
-            return res.status(400).send({ error: 'No ingreso el nombre del Técnico.' });
-        }
-        if (!dni) {
-            return res.status(400).send({ error: 'No ingreso el dni del Técnico.' });
-        }
-        if (!telefono) {
-            return res.status(400).send({ error: 'No ingreso el telefono del Técnico.' });
-        }
-        if (!correo) {
-            return res.status(400).send({ error: 'No ingreso el correo del Técnico.' });
-        }
-        if (!direccion) {
-            return res.status(400).send({ error: 'No ingreso la dirección del Técnico.' });
-        }
-        if (!tipo) {
-            return res.status(400).send({ error: 'No especifico el tipo de Técnico.' });
-        }
+    let tecnicoJSON = fs.readFileSync('datos/tecnicos.json', 'utf8');
 
-        const updatedTecnico = {
-            id: Number(id),
-            nombre,
-            dni,
-            telefono,
-            correo,
-            direccion,
-            tipo,
-        };
-        tecnicos[tecnicoIndex] = updatedTecnico;
+    let tecnicos = JSON.parse(tecnicoJSON);
+    if (!tecnicos) return res.status(400).json('Json Inexistente.');
 
-        fs.writeFileSync('datos/tecnicos.json', JSON.stringify(tecnicos), {
-            encoding: 'utf8',
-            flag: 'w',
-        });
+    let tecnicoIndex = tecnicos.findIndex(
+      (tecnico) => Number(tecnico.id) === Number(id)
+    );
 
-        return res.status(200).json(tecnicos);
-    } catch (error) {
-        console.error(error);
-        return res.status(404).json('Error Interno del Servidor.');
+    if (tecnicoIndex === -1) {
+      return res
+        .status(400)
+        .send({ error: `No existe el Técnico con id: ${tecnicoId}.` });
     }
-};
+    if (!nombre) {
+      return res
+        .status(400)
+        .send({ error: 'No ingreso el nombre del Técnico.' });
+    }
+    if (!dni) {
+      return res.status(400).send({ error: 'No ingreso el dni del Técnico.' });
+    }
+    if (!telefono) {
+      return res
+        .status(400)
+        .send({ error: 'No ingreso el telefono del Técnico.' });
+    }
+    if (!correo) {
+      return res
+        .status(400)
+        .send({ error: 'No ingreso el correo del Técnico.' });
+    }
+    if (!direccion) {
+      return res
+        .status(400)
+        .send({ error: 'No ingreso la dirección del Técnico.' });
+    }
+    if (!tipo) {
+      return res
+        .status(400)
+        .send({ error: 'No especifico el tipo de Técnico.' });
+    }
 
+    const updatedTecnico = {
+      id: Number(id),
+      nombre,
+      dni,
+      telefono,
+      correo,
+      direccion,
+      tipo
+    };
+    tecnicos[tecnicoIndex] = updatedTecnico;
+
+    fs.writeFileSync('datos/tecnicos.json', JSON.stringify(tecnicos), {
+      encoding: 'utf8',
+      flag: 'w'
+    });
+
+    return res.status(200).json(tecnicos);
+  } catch (error) {
+    console.error(error);
+    return res.status(404).json('Error Interno del Servidor.');
+  }
+};
 
 // DELETE A tecnico By ID
 exports.deleteTecnico = async (req, res) => {
-    try {
-        const tecnicoId = req.params.tecnicoId;
-        let tecnicoJSON = fs.readFileSync('datos/tecnicos.json', 'utf8');
-        let tecnicos = JSON.parse(tecnicoJSON);
+  try {
+    const tecnicoId = req.params.tecnicoId;
+    let tecnicoJSON = fs.readFileSync('datos/tecnicos.json', 'utf8');
+    let tecnicos = JSON.parse(tecnicoJSON);
 
-        if (!tecnicos) return res.status(400).json('Json Inexistente.');
+    if (!tecnicos) return res.status(400).json('Json Inexistente.');
 
-        let tecnicoIndex = tecnicos.findIndex(
-            (tecnico) => Number(tecnico.id) === Number(tecnicoId)
-        );
+    let tecnicoIndex = tecnicos.findIndex(
+      (tecnico) => Number(tecnico.id) === Number(tecnicoId)
+    );
 
-        if (tecnicoIndex === -1) {
-            return res.status(400).send({ error: `No existe el tecnico con id: ${tecnicoId} .` });
-        }
-
-        tecnicos.splice(tecnicoIndex, 1);
-
-        fs.writeFileSync('datos/tecnicos.json', JSON.stringify(tecnicos), {
-            encoding: 'utf8',
-            flag: 'w',
-        });
-
-        return res.status(200).json(tecnicos);
-    } catch (error) {
-        console.error(error);
-        return res.status(404).json('Error Interno del Servidor.');
+    if (tecnicoIndex === -1) {
+      return res
+        .status(400)
+        .send({ error: `No existe el tecnico con id: ${tecnicoId} .` });
     }
+
+    tecnicos.splice(tecnicoIndex, 1);
+
+    fs.writeFileSync('datos/tecnicos.json', JSON.stringify(tecnicos), {
+      encoding: 'utf8',
+      flag: 'w'
+    });
+
+    return res.status(200).json(tecnicos);
+  } catch (error) {
+    console.error(error);
+    return res.status(404).json('Error Interno del Servidor.');
+  }
 };
